e5eca7660dd7e3d639c7d7a0fa34954aed48342a
mdadsetan <mehran.dadsetan.md@gmail.com>
2024-11-04 17:37:29 -0500

    langchain foundations

--- SerpTool.py
+++ SerpTool.py
@@ -4,7 +4,7 @@ import os
 from langchain.llms import OpenAI
 from langchain.agents import AgentType, initialize_agent, load_tools
 
-os.environ["SERPAPI_API_KEY"] = "4e7c0c06f2695b2f953f8ba029a82b5662b171937dd352be5a9ea1d7633fae8f"
+os.environ["SERPAPI_API_KEY"] = "??"
 
 # Initialize LocalLLM with the specified endpoint URL
 llm = SLM.LocalLLM(endpoint_url="http://localhost:5001/chat")
